9d0168a68a93ef6e7ba80b4c968d269f2d3eb80e
Łukasz <lukaszkogut1996@gmail.com>
2024-02-05 22:01:01 +0100

    I ruined everything / styling from scratch / new idea

--- index.js
+++ index.js
@@ -63,18 +63,3 @@ function refreshWeather() {
 getWeatherData()
 
 document.getElementById('refresh-button').addEventListener('click', refreshWeather)
-
-// ----------------ProtoType-------DM---------
-
-function switchMode() {
-	let moon = document.getElementById('moon')
-	if (moon.className == 'moon') {
-		moon.className = 'sun'
-		document.body.style.backgroundColor = '#141D26'
-		document.body.style.color = '#fff'
-	} else {
-		moon.className = 'moon'
-		document.body.style.backgroundColor = '#fff'
-		document.body.style.color = '#000'
-	}
-}
