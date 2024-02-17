3b9e944dd4725ca954ab3f5dc2c7584630f1dc83
Łukasz <lukaszkogut1996@gmail.com>
2024-02-17 23:14:03 +0100

    added scroll up

--- index.js
+++ index.js
@@ -68,3 +68,26 @@ function refreshWeather() {
 getWeatherData()
 
 document.getElementById('refresh-button').addEventListener('click', refreshWeather)
+
+
+// --------------scroll--------------
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const scrollToTopButton = document.querySelector('.scroll-to-top');
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 300) {
+            scrollToTopButton.style.display = 'block';
+        } else {
+            scrollToTopButton.style.display = 'none';
+        }
+    });
+
+    scrollToTopButton.addEventListener('click', function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
